4fd0800ba6c29115f27eb1c7f67c7884443715db
Prajun Trital <prajuncs@gmail.com>
2023-02-13 22:35:56 -0600

    adding image and details components

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -2,13 +2,13 @@ import Route from '@ember/routing/route';
 
 export default class CartRoute extends Route {
   model() {
-    const items = [ 
-      { 
-        'price': 10,
+    const items = [
+      {
+        price: 10,
       },
-      { 
-        'price': 15,
-      } 
+      {
+        price: 15,
+      },
     ];
     return items;
   }
